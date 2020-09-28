bf36ed70dd706a04c076fe623a0e4404d68e2657
Matthew McDevitt <mmcdevitt1997@gmail.com>
2020-09-28 08:16:43 -0500

    the bot can reply to different questions

--- main.py
+++ main.py
@@ -10,6 +10,24 @@ def speak(text):
     tts.save(filename)
     playsound.playsound(filename)
 
+def get_audio(): 
+    r = sr.Recognizer()
+    with sr.Microphone() as source: 
+        audio = r.listen(source)
+        said = ""
 
-speak("hello there")
+        try: 
+            said = r.recognize_google(audio)
+            print(said)
+        except Exception as e: 
+            print ("Exception " + str(e))
+    
+    return said 
+
+
+
+text = get_audio()
+
+if "hello" in text:
+    speak("hello, how are you?")
 
